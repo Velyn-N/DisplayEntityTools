b33cec828249568e098386ee0f62d0c81e819412
Velyn-N <nils01.gaming@gmail.com>
2023-09-23 17:27:18 +0200

    Improved Plugin Info

--- src/main/java/de/nmadev/textdisplaytools/command/subcommands/InfoSubCommand.java
+++ src/main/java/de/nmadev/textdisplaytools/command/subcommands/InfoSubCommand.java
@@ -6,9 +6,14 @@ import de.nmadev.textdisplaytools.command.BaseCommand;
 import io.papermc.paper.plugin.configuration.PluginMeta;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+
+import java.util.Objects;
 
 public class InfoSubCommand extends BaseCommand {
 
@@ -27,23 +32,33 @@ public class InfoSubCommand extends BaseCommand {
         String headlineText = "--- " + pluginMeta.getName() + " Plugin Info ---";
         TextComponent headline = Component.text(headlineText, NamedTextColor.GOLD).decorate(TextDecoration.BOLD);
 
-        String pluginDesc = pluginMeta.getDescription();
-        Component description = Component.text(pluginDesc == null ? "" : pluginDesc, NamedTextColor.YELLOW);
+        Component description = Component.text(Objects.requireNonNull(pluginMeta.getDescription()), NamedTextColor.YELLOW);
 
         Component version = getLabeledValue("Version", pluginMeta.getVersion());
         Component authors = getLabeledValue("Authors", String.join(", ", pluginMeta.getAuthors()));
         Component contributors = getLabeledValue("Contributors", String.join(", ", pluginMeta.getContributors()));
-        Component website = getLabeledValue("Website", pluginMeta.getWebsite());
+        Component website = createWebsiteComponent(pluginMeta.getWebsite());
 
-        TextComponent result = Component.empty().append(headline).append(Component.newline());
-        if (pluginDesc != null) {
-            result = result.append(description).append(Component.newline());
-        }
-        result = result
-                    .append(version).append(Component.newline())
-                    .append(authors).append(Component.newline())
-                    .append(contributors).append(Component.newline())
-                    .append(website).append(Component.newline());
+        Component footer = Component.text("--- --- --- --- --- --- --- --- ---", NamedTextColor.GOLD)
+                                    .decorate(TextDecoration.BOLD);
+
+        TextComponent result = Component.empty()
+                .append(headline)
+                .append(Component.newline())
+                .append(description)
+                .append(Component.newline())
+                .append(version)
+                .append(Component.newline())
+                .append(authors)
+                .append(Component.newline())
+                .append(contributors)
+                .append(Component.newline())
+                .append(website)
+                .append(Component.newline())
+                .append(Component.newline())
+                .append(createEnvInfoComponent(pluginMeta.getName(), pluginMeta.getVersion()))
+                .append(Component.newline())
+                .append(footer);
 
         sender.sendMessage(result);
         return true;
@@ -52,5 +67,26 @@ public class InfoSubCommand extends BaseCommand {
     private Component getLabeledValue(String label, String value) {
         return Component.text(label + ": ", NamedTextColor.GOLD)
                         .append(Component.text(value, NamedTextColor.YELLOW));
+    }
+
+    private Component createWebsiteComponent(String website) {
+        return Component.text( "Website: ", NamedTextColor.GOLD)
+                .append(Component.text(website, NamedTextColor.YELLOW).decorate(TextDecoration.UNDERLINED))
+                .clickEvent(ClickEvent.openUrl(Objects.requireNonNull(website)))
+                .hoverEvent(HoverEvent.showText(Component.text("Click to open Link")));
+    }
+
+    private Component createEnvInfoComponent(String pluginName, String pluginVersion) {
+        String serverSoftware = Bukkit.getName();
+        String serverVersion = Bukkit.getVersion();
+        String jdkVersion = System.getProperty("java.version");
+
+        String fullInfo = String.format("%s Version: %s%nServer Software: %s%nServer Version: %s%nJDK Version: %s",
+                pluginName, pluginVersion, serverSoftware, serverVersion, jdkVersion);
+
+        return Component.text("Copy Environment Information", NamedTextColor.YELLOW)
+                .decorate(TextDecoration.UNDERLINED)
+                .hoverEvent(HoverEvent.showText(Component.text("Click to copy Environment Information to Clipboard")))
+                .clickEvent(ClickEvent.copyToClipboard(fullInfo));
     }
 }
